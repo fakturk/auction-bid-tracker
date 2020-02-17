bf565ee388c1ee6e3507ca0360210ad241580d2e
fakturk <f.akturk@gmail.com>
2020-02-17 18:29:53 +0300

    addUser() and addUserWithName methods implemented

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import(
 	"github.com/gorilla/mux"
 	"net/http"	
 	"fmt"
+	"encoding/json"
+	"math/rand"
+  	"strconv"
 )
 
 type User struct {
@@ -31,10 +34,36 @@ var bids []Bid
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", addUser).Methods("POST")
+	router.HandleFunc("/users/{name}", addUserWithName).Methods("POST")
 	http.ListenAndServe(":8000", router)
 
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"GetUsers")
+	// fmt.Fprintf(w,"GetUsers\n")
+	w.Header().Set("Content-Type", "application/json")
+  	json.NewEncoder(w).Encode(users)
+  }
+
+  func addUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(user)
+	user.ID = strconv.Itoa(rand.Intn(1000000))
+	users = append(users, user)
+	json.NewEncoder(w).Encode(&user)
+  }
+
+
+  func addUserWithName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	fmt.Println(params)
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(user)
+	user.ID = strconv.Itoa(rand.Intn(1000000))
+	user.Name = params["name"]
+	users = append(users, user)
+	json.NewEncoder(w).Encode(&user)
   }
